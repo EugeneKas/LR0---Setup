59529f2d470ec8a7fd18bbc7065db60a8e1a063c
EugeneKas <79342122+EugeneKas@users.noreply.github.com>
2021-02-28 18:07:50 +0300

    Add

---
+++ Tests.py
@@ -0,0 +1,9 @@
+import math
+
+a = int(input())
+b = int(input())
+
+print("Сумма a и b равна " + str(a + b))
+print("Разность a и b равна " + str(a - b))
+print("Произведение a и b равно " + str(a * b))
+print("Квадратный корень чисел a и b равны " + str(math.sqrt(a)) + " и " + str(math.sqrt(b)) + " соответственно")
